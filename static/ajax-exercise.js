d41aced7ada7098385247059342664aadd8d6b7f
Kristal <kristalkung@gmail.com>
2021-03-27 20:00:18 +0000

    completed part 1 and part 2

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -6,12 +6,12 @@
 function showFortune(evt) {
 
     // TODO: get the fortune and show it in the #fortune-text div
+    $.get('/fortune', (FORTUNES) => {
+        $('#fortune-text').html(FORTUNES);
+    });
 }
 
 $('#get-fortune-button').on('click', showFortune);
-
-
-
 
 
 // PART 2: SHOW WEATHER
@@ -24,11 +24,13 @@ function showWeather(evt) {
 
 
     // TODO: request weather with that URL and show the forecast in #weather-info
+    $.get(url, formData, (response) => {
+        $('#weather-info').html(response.forecast);
+        console.log('this is the response');
+    });
 }
 
 $("#weather-form").on('submit', showWeather);
-
-
 
 
 // PART 3: ORDER MELONS
@@ -38,6 +40,15 @@ function orderMelons(evt) {
 
     // TODO: show the result message after your form
     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+
+    const formInputs = {
+        'melon_type': $('#melon-type-field').val(),
+        'qty': $('#qty-field').val()
+    };
+    
+    $.post('/order-melons.json', formInputs, (response) => {
+        $('#order-status').html(response);
+    });
 }
 
 $("#order-form").on('submit', orderMelons);
